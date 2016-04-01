f8720dc2d2034639efbf84420976a3d414428043
Yiyuan Zhang <markzhang90@gmail.com>
2016-04-01 18:03:58 +0800

    authuser

---
+++ dj_onestory/authuser/models.py
@@ -0,0 +1,90 @@
+from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
+
+
+class OneStoryUserManager(BaseUserManager):
+
+    def create_user(self, email, date_of_birth, password=None, phone=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            date_of_birth=date_of_birth,
+            phone=phone
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, date_of_birth, password, phone):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(email,
+            password=password,
+            date_of_birth=date_of_birth,
+            phone=phone,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class OneStoryUser(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    phone = models.BigIntegerField(unique=True)
+    date_of_birth = models.DateField()
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    first_name = models.TextField(max_length=255)
+    last_name = models.TextField(max_length=255)
+    nick_name = models.CharField(max_length=20)
+    personal_id = models.CharField(max_length=20)
+    create_time = models.DateTimeField(auto_now=True)
+
+    objects = OneStoryUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['date_of_birth', 'phone',]
+
+    @property
+    def get_full_name(self):
+        # The user is identified by their email address
+        return self.last_name+self.first_name
+
+    @property
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.first_name
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
---
+++ dj_onestory/onestory/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OnestoryConfig(AppConfig):
+    name = 'onestory'
